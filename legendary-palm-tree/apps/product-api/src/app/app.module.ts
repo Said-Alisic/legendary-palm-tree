228ff84eef70730a12ad79bc8a6959cc7ca0101f
Said-Alisic <said@shieldapp.ai>
2022-11-29 00:14:03 +0100

    feat: 🎸 (product-api) Import new modules into core app module
    Import products and product pricings modules to connect the controllers
    and services with the consumers.

--- legendary-palm-tree/apps/product-api/src/app/app.module.ts
+++ legendary-palm-tree/apps/product-api/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { DatabaseModule } from '@libs/application';
+import { ProductPricingsModule, ProductsModule } from './controllers';
 
 @Module({
-  imports: [CoreModule, DatabaseModule],
+  imports: [CoreModule, DatabaseModule, ProductPricingsModule, ProductsModule],
   controllers: [],
   providers: [],
 })
---
+++ legendary-palm-tree/apps/product-api/src/app/controllers/index.ts
@@ -0,0 +1,2 @@
+export { ProductPricingsModule } from './product-pricings/product-pricings.module';
+export { ProductsModule } from './products/products.module';
